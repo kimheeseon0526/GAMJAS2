b72e8fba065df2f61e574d1e11f0936671174249
kimchan <kimchan9408@gmail.com>
2025-07-01 18:06:15 +0900

    domain.info package 생성, RecommendPlace 생성, domain.en package 생성 및 RecommendContentType Enum 생성, mapper package 생성

---
+++ src/main/java/domain/info/RecommendPlace.java
@@ -0,0 +1,15 @@
+package domain.info;
+
+import domain.en.RecommendContentType;
+
+public class RecommendPlace {
+	private Long recomNo;
+	private RecommendContentType recomContenttype ;
+	private String recomPlaceId;
+	private String apiSubcontent;
+	private String stationId;
+	private Long memNo;
+	private String regdatetime;
+	private String moddatetime;
+	
+}
