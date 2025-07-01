bd3f318182f8ffc889889997119251b1c1a037ec
jiyu196 <jiyu16413@gmail.com>
2025-07-01 18:59:18 +0900

    domain > Attach, Board, Category, Reply 생성

---
+++ src/main/java/domain/Board.java
@@ -0,0 +1,31 @@
+package domain;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Alias("board")
+public class Board {
+
+	private Long bno;
+	private String title;
+	private String content;
+	private String id;
+	private String regdate; // 작성일
+	private String moddate; // 수정일
+	private Integer cnt; // 조회수 넘버
+	private Integer cno; // 카테고리 넘버
+
+
+}
